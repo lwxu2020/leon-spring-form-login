9dbc3076f1495050ebaf1e3dfe1bce5267d82679
Liangwu Xu <Liangwu.Xu@gmail.com>
2024-09-30 23:10:28 -0500

    first commit

---
+++ src/main/java/com/leon/myapp/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.leon.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+   @GetMapping("/login")
+   public String login() {
+      return "login";
+   }
+
+   @GetMapping("/")
+   public String dashboard() {
+      return "dashboard";
+   }
+}
